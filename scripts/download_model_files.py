102aeb1758f2e260072f99e4b1fdea9d1abb6630
James Adams <monocongo@gmail.com>
2019-06-24 23:40:53 -0400

    added comments/documentation, code to create non-existent destination directory, etc.
    #5

--- scripts/download_model_files.py
+++ scripts/download_model_files.py
@@ -7,7 +7,23 @@ import wget
 
 # ------------------------------------------------------------------------------
 def main(destination_dir: str) -> int:
+    """
+    Download colorization model files provided by Richard Zhang et al.
+    See https://richzhang.github.io/colorization/
 
+    :param destination_dir: destination directory location for downloaded files
+    :return: 0 if all goes as expected
+    """
+
+    # make sure directory exists, otherwise create it
+    if not os.path.isdir(destination_dir):
+        if os.path.exists(destination_dir):
+            message = f"{destination_dir} already exists but is not a directory"
+            raise ValueError(message)
+        else:
+            os.mkdir(destination_dir)
+
+    # download model files from relevant URLs
     urls = [
         "https://github.com/richzhang/colorization/blob/master/colorization/resources/pts_in_hull.npy?raw=true",
         "https://raw.githubusercontent.com/richzhang/colorization/master/colorization/models/colorization_deploy_v2.prototxt",
@@ -17,6 +33,7 @@ def main(destination_dir: str) -> int:
         filename = wget.download(url)
         shutil.move(filename, os.sep.join((destination_dir, filename)))
 
+    # successful completion
     return 0
 
 
