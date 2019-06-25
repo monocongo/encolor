b16397e8fdd3cf67021701a4b575f10e254a9b9d
James Adams <monocongo@gmail.com>
2019-06-24 23:29:00 -0400

    added URLs for files to download
    #5

--- scripts/download_model_files.py
+++ scripts/download_model_files.py
@@ -1,10 +1,21 @@
 import argparse
+import os
+import shutil
 
 import wget
 
 
 # ------------------------------------------------------------------------------
 def main(destination_dir: str) -> int:
+
+    urls = [
+        "https://github.com/richzhang/colorization/blob/master/colorization/resources/pts_in_hull.npy?raw=true",
+        "https://raw.githubusercontent.com/richzhang/colorization/master/colorization/models/colorization_deploy_v2.prototxt",
+        "http://eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/colorization_release_v2.caffemodel"
+    ]
+    for url in urls:
+        filename = wget.download(url)
+        shutil.move(filename, os.sep.join((destination_dir, filename)))
 
     return 0
 
